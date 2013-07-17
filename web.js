0878cea02a0b4086ad27817364ab8e2bd53e358c
Gian Carlo Salvati <gian@salvati.com.br>
2013-07-17 00:56:34 +0000

    Exercise todo

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
 var express = require('express');
+var fs = require('fs');
+var buffer = new Buffer(100);	
+
+fs.readFileSync('index.html', function( err, data) 
+	{
+		buffer.write(data);
+	}
+);
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+app.get('/', function(request, response) 
+	{
+		response.send(buffer.toString());
+	}
+);
+	
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, function() 
+	{
+	  console.log("Listening on " + port);
+	}
+);
